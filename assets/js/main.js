7b39f6019ee882eee1f33871a2be1589c62796f4
Sara Esgerova <sara.esgerova2@gmail.com>
2016-08-16 14:13:26 +0400

    change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,48 @@
+jQuery(document).ready(function() {
+
+//----preloader---------
+    var deg=0;
+	$(window).load(function() {   
+		$('#circle')
+			var rotate=setInterval(function(){
+				deg=deg+60;
+				$('#circle').css({'transform':'rotate('+deg+'deg)'});
+			},120)
+			
+			$(this)
+			.animate({
+				'transform':'rotate('+deg+'deg)'
+				},500, function() {clearInterval(rotate);})
+			.delay(500)
+			.fadeOut('slow');
+						
+		$('#preloader')
+			.delay(500)
+			.fadeOut('slow');
+	    $('body')
+	   		.delay(500)
+	    	.css({'overflow':'visible'});
+	});
+
+
+//--------dropdown menu-----
+
+//----button tabs --->>-- first tab, second tab, third tab
+var tabselect=$('#about .first-part .btngroup a');
+var groupselect=$('#about .first-part .group');
+
+	$(tabselect).click(function() {
+		$(tabselect).removeClass('active');
+		$(this).addClass('active');
+
+		var data=$(this).data('href');
+		$(groupselect).css({
+			display: 'none'
+		});
+		$(data).css({
+			display: 'block'
+		});
+		
+	});
+});
 
